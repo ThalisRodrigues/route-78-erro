5f0d523bd803685e4ab47f8a5afbb8044eb295c0
MitradirTheLastMexicano <38816065+MitradirTheLastMexicano@users.noreply.github.com>
2018-06-09 23:12:49 -0300

    Add files via upload

--- lojaBike/cadastrar_bike.php
+++ lojaBike/cadastrar_bike.php
@@ -1,65 +1,40 @@
-<h1>Cadastrar Bike</h1>
+<h1>Cadastrar Produto</h1>
 <form action="index.php?page=salvar_bike&acao=cadastrar" method="POST">
-	<!-- <div class="form-group">
-		<label>Marca</label>
-		< ?php
-			$sql = "SELECT * FROM produto";
 	
-			$result = $conn->query($sql);
-			
-			$qtd = $result->num_rows;
-			
-			if($qtd > 0){
-				print "<p>Encontrou <b>$qtd</b> resultado(s)</p>";
-				print "<table class='table table-bordered table-striped table-hover'>";
-				print "<tr>";
-				print "<th>Codigo Marca</th>";
-				print "<th>Nome da Marca</th>";
-				print "</tr>";
-				while( $row = $result->fetch_assoc() ){
-					print "<tr>";
-					print "<td>".$row["id_marca"]."</td>";
-					print "<td>".$row["nome_marca"]."</td>";
-					print "</tr>";
-				}
-				print "</table>";
-			}else{
-				print "Não encontrou resultados";
-			}
-		?>-->
-		
-		<!-- `codigo_produto` INT NOT NULL AUTO_INCREMENT,
-  `nome_produto` VARCHAR(45) NOT NULL,
-  `descricao_produto` TEXT(500) NOT NULL,
-  `estoque_produto` VARCHAR(45) NOT NULL,
-  `valor_produto` DOUBLE NOT NULL,
-  `situacao_produto` VARCHAR(45) NOT NULL,-->
+	<div class="row">
+		<div class="form-group col-md-12">
+			<label>Nome Produto</label>
+			<input type="text" name="nome_produto" class="form-control">
+			<br/>
+		</div>
 	</div>
-
-	<div class="form-group">
-		<label>Nome Produto</label>
-		<input type="text" name="nome_produto" class="form-control">
-		<br/>
+	<div class="row">
+		<div class="form-group col-md-12">
+			<label>Descrição</label>
+			<input type="text" name="descricao_produto" class="form-control">
+			<br/>
+		</div>
 	</div>
-	<div class="form-group">
-		<label>Descrição</label>
-		<input type="text" name="descricao_produto" class="form-control">
-		<br/>
+	<div class="row">
+		<div class="form-group col-md-12">
+			<label>Estoque</label>
+			<input type="text" name="estoque_produto" class="form-control">
+			<br/>
+		</div>
 	</div>
-	<div class="form-group">
-		<label>Estoque</label>
-		<input type="text" name="estoque_produto" class="form-control">
-		<br/>
+	<div class="row">
+		<div class="form-group col-md-12">
+			<label>Valor</label>
+			<input type="number" name="valor_produto" class="form-control">
+			<br/>
+		</div>
 	</div>
-	<div class="form-group">
-		<label>Valor</label>
-		<input type="number" name="valor_produto" class="form-control">
-		<br/>
-	</div>
-	<div class="form-group">
-		<label>Situação Produto</label>
-		<input type="text" name="situacao_produto" class="form-control">
-		<br/>
+	<div class="row">
+		<div class="form-group col-md-12">
+			<label>Situação Produto</label>
+			<input type="text" name="situacao_produto" class="form-control">
+			<br/>
+		</div>
 	</div>
 	
 	
--- lojaBike/cadastrar_cliente.php
+++ lojaBike/cadastrar_cliente.php
@@ -1,30 +1,41 @@
 <h1>Cadastrar Cliente</h1>
 <form action="index.php?page=salvar_cliente&acao=cadastrar" method="POST">
-	
-	<div class="form-group">
+	<div class="row">
+		<div class="form-group col-md-12">
 			<label>Nome Cliente</label>
 			<input type="text" name="nome_cliente" class="form-control" >
 		</div>
-		<div class="form-group">
+	</div>
+	<div class="row">
+		<div class="form-group col-md-12">
 			<label>Tipo</label>
 			<input type="text" name ="tipo_cliente" class="form-control" >
 		</div>
-		<div class="form-group">
+	</div>
+	<div class="row">
+		<div class="form-group col-md-12">
 			<label>CPF</label>
 			<input type="text" name="cpf_cliente" class="form-control" >
 		</div>
-		<div class="form-group">
+	</div>
+	<div class="row">
+		<div class="form-group col-md-12">
 			<label>Telefone</label>
 			<input type="text" name="telefone_cliente" class="form-control" >
 		</div>
-		<div class="form-group">
+	</div>
+	<div class="row">
+		<div class="form-group col-md-12">
 			<label>Email</label>
 			<input type="text" name="email_cliente" class="form-control" >
 		</div>
-		<div class="form-group">
+	</div>
+	<div class="row">
+		<div class="form-group col-md-12">
 			<label>Endereço</label>
 			<input type="text" name="endereco_cliente" class="form-control" >
-		</div>	
+		</div>
+	</div>		
 	
 	
 	<div class="form-group">
